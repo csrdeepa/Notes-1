28275b89b2e30171c11a4481cdccf6a195c7969d
Deepa <79695313+osoftzwpgit@users.noreply.github.com>
2021-07-30 11:11:52 +0530

    Create class-sz-auth.php

---
+++ woocommerce_api_for_mobile_all/includes/class-sz-auth.php
@@ -0,0 +1,19 @@
+<?php
+class Sz_Auth
+{
+    public function __construct()
+    {
+        // $this->plugin_name = 'sz-auth';
+        // $this->version = '0.0.0';
+
+        // $this->load_dependencies();
+        // $this->set_locale();
+        // $this->define_public_hooks();
+    }
+    public function run()
+    {
+        // $this->loader->run();
+    }
+
+
+}
